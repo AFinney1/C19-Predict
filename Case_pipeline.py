a51753679a353b8c8e29cde4d9dcc903b672c3df
AFinney1 <amfinney792@gmail.com>
2020-07-06 20:57:44 -0500

    weekend changes

--- Case_pipeline.py
+++ Case_pipeline.py
@@ -1,9 +1,26 @@
 import pandas as pd 
 import numpy as np 
-import conducto
+#import conducto as C
+from tkinter import Tk
+import streamlit as st
+from tkinter import filedialog
+import Tk
 
 def get_cases():
-    case_df = pd.read_csv("/Local_case_data/")
+    f = filedialog.askopenfilename(initialdir = "C19-Predict",
+                                        title = "Select a File",
+                                        filetypes = (("CSV files",
+                                        "*.csv*"),
+                                        ("all files",
+                                        "*.*")))
+    #label_file_explorer.configure(text="File Opened: "+f)
+    window = Tk()
+    window.title("Case data explorer")
+    case_df = pd.read_csv("/Local_case_data/"+str(f))
+    return case_df
+
+
+get_cases()
 
 
 
