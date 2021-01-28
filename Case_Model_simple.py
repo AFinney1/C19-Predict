e7cc848b36575d412fd7d07bb2540689f9e79964
afinney1 <amfinney792@gmail.com>
2021-01-28 09:34:06 -0600

    More changes

--- Case_Model_simple.py
+++ Case_Model_simple.py
@@ -223,7 +223,7 @@ print(f'Window shape: {test_window.shape}')
 model = tf.keras.Sequential()
 # Add an Embedding layer expecting input vocab of size 1000, and
 # output embedding dimension of size 64.
-model.add(layers.Embedding(input_dim=1000, output_dim=64))
+model.add(layers.Embedding(input_dim=1000, output_dim=64), gra)
 # Add a LSTM layer with 128 internal units.
 model.add(layers.LSTM(128))
 # Add a Dense layer with 10 units.
