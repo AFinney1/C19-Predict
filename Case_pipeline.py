fb5b5fe3426d2040536b3df70fdefe166d272abc
afinney1 <amfinney792@gmail.com>
2021-06-26 11:33:13 -0500

    get_cases retrieves most recent confirmed cases

--- Case_pipeline.py
+++ Case_pipeline.py
@@ -5,15 +5,23 @@ import numpy as np
 import tkinter
 from tkinter import filedialog
 import os
+import requests
+import re
 
-#Need to add code to automatically update covid case data
+
 def get_cases():
+    '''
     window = tkinter.Tk()
     window.title("Case data explorer")
     window.withdraw()
+    '''
     cwd = os.getcwd()
-    #f = cwd+"/Local_case_data/time_series_covid19_confirmed_US.csv"
-    f = filedialog.askopenfilename(initialdir="/Local_case_data/",initialfile="time_series_covid19_confirmed_US.csv", title="Select a File",filetypes=(("CSV files", "*.csv*"),("all files", "*.*")), )
+    csv_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"
+    req = requests.get(csv_url, allow_redirects=True)
+    open('Local_case_data/time_series_covid19_confirmed_US.csv', 'wb').write(req.content)
+    #f = filedialog.askopenfilename(initialdir="/Local_case_data/",initialfile="time_series_covid19_confirmed_US.csv", title="Select a File",filetypes=(("CSV files", "*.csv*"),("all files", "*.*")), )
+    cwd = os.getcwd()
+    f = cwd+"/Local_case_data/time_series_covid19_confirmed_US.csv"
     #label_file_explorer.configure(text="File Opened: "+f)
     #f.close()
     case_df = pd.read_csv(str(f))
@@ -23,4 +31,3 @@ def get_cases():
 
 
 test = get_cases()
-#need to write actual tests
